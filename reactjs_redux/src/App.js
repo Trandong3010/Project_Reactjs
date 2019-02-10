3094bacbac8c736666a043af45f756edcd7b0623
Trandong.o1o0@gmail.com <trandong.o1o0@gmail.com>
2019-02-10 13:20:12 +0700

    a

--- reactjs_redux/src/App.js
+++ reactjs_redux/src/App.js
@@ -4,6 +4,47 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var redux = require('redux');
+    var oldState = {
+      num: ["màn hình", "chuột", "bàn phím"],
+      editStatus: true
+    }
+
+    var reducer1 = (state = oldState, action) => {
+      switch (action.type) {
+        case "CHANGE_EDIT_STATUS":
+          return { ...state, editStatus: !state.editStatus }
+          break;
+        case "ADD_NEW":
+          return { ...state, num: [...state.num, action.newItem] }
+          break;
+        case "DELETE":
+          return { ...state, num: state.num.filter((value, i) => i != action.number) }
+          break;
+        default:
+          break;
+      }
+      return state;
+    }
+
+
+
+    var store1 = redux.createStore(reducer1);
+    store1.subscribe(() => {
+      console.log(store1.getState());
+    })
+    store1.dispatch({ type: "CHANGE_EDIT_STATUS" })
+    store1.dispatch({
+      type: "ADD_NEW",
+      newItem: "Tai nghe"
+    })
+    store1.dispatch({
+      type: "DELETE",
+      number: 3
+    })
+
+
+    
     return (
       <div className="App">
         <header className="App-header">
